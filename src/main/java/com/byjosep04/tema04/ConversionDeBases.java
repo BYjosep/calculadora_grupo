c1be459b2cd5d29d73bf742e1a8c32295e8d13d6
JoseHS2002 <joherso16@gmail.com>
2024-11-06 17:11:40 +0100

    Update ConversionDeBases.java

--- src/main/java/com/byjosep04/tema04/ConversionDeBases.java
+++ src/main/java/com/byjosep04/tema04/ConversionDeBases.java
@@ -2,14 +2,30 @@ package com.byjosep04.tema04;
 
 public class ConversionDeBases {
   /*
-  De decimal a binario
+1.De decimal a binario
   Do(n%2...n%2)
   while(n%2>1)
   n/2
 
-  De decimal a hexadecimal
+2.De decimal a hexadecimal
+  Do(n%16...n%16)
+if(n%16==10){
+10='A'
+} else if(n%16==11){
+11='B'
+} else if(n%16==12){
+12='C'
+} else if(n%16==13){
+13='D'
+} else if(n%16==14){
+14='E'
+} else if(n%16==15){
+15='F'
+}
+  while(n%16>1)
+  n/16
 
-  De binario a decimal
+3.De binario a decimal
   n*2^7+n*2^6+n*2^5+n*2^4+n*2^3+n*2^2+n*2^1+n*2^0
   */
 }
