29f1588a309e5a656c7a86ff654304448cfc3615
JoseHS2002 <joherso16@gmail.com>
2024-11-06 16:30:46 +0100

    Update Operaciones_aritmeticas.java

--- src/main/java/com/byjosep04/tema04/Operaciones_aritmeticas.java
+++ src/main/java/com/byjosep04/tema04/Operaciones_aritmeticas.java
@@ -1,8 +1,5 @@
 package com.byjosep04.tema04;
 
 public class Operaciones_aritmeticas {
-  sumar
-  restar
-  multiplicar
-  dividir
+  sumar(x+y); restar(x-y); multiplicar(x*y); dividir(x/y); resto(x%y)
 }
