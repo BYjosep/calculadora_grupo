2e61ef8928b6bc54a165db648b3a434c7d477bc7
JoseHS2002 <joherso16@gmail.com>
2024-11-06 16:42:55 +0100

    Update ConversionDeBases.java

--- src/main/java/com/byjosep04/tema04/ConversionDeBases.java
+++ src/main/java/com/byjosep04/tema04/ConversionDeBases.java
@@ -1,4 +1,11 @@
 package com.byjosep04.tema04;
 
 public class ConversionDeBases {
+  De decimal a binario
+  Do(n%2...n%2)
+  while(n%2>1)
+  n/2
+
+  De binario a decimal
+  n*2^7+n*2^6+n*2^5+n*2^4+n*2^3+n*2^2+n*2^1+n*2^0
 }
