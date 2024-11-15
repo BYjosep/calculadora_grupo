18c9ca03caed35f6f635d0c57fa964d4b7ab3f76
Adrian Hustea <adriannicolash3@gmail.com>
2024-11-16 00:53:32 +0100

    Crear clasee BinarioHexadecimal

--- src/main/java/com/byjosep04/tema04/DecimalHexadecimal.java
+++ src/main/java/com/byjosep04/tema04/DecimalHexadecimal.java
@@ -21,24 +21,24 @@ public class DecimalHexadecimal {
     public static String decimalAHexadecimal(int numero) {
         StringBuilder resultado = new StringBuilder();
 
-        // Manejo de casos negativos
+
         if (numero == 0) return "0";
         if (numero < 0) {
             return "-" + decimalAHexadecimal(-numero);
         }
 
-        // Un array con todos los numeros hexadecimales
+
         char[] hexChar = {'0', '1', '2', '3', '4', '5', '6', '7',
                 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-        // Conversión a hexadecimal
+
         while (numero > 0) {
             int residuo = numero % 16;
             resultado.append(hexChar[residuo]);
             numero /= 16;
         }
 
-        // Revertir el resultado ya que los dígitos se construyen al revés
+
         return resultado.reverse().toString();
     }
 }
