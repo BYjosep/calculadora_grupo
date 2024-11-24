7f39127343a98fa7e641c0d814ef034a4660eb51
Alvaro Campos <alvaro.campos.bolufer.a@gmail.com>
2024-11-24 16:00:34 +0100

    Commit del Java class de BinarioDecimal.java, cambios en la parte de binario a decimal metodo llamado BinarioDecimal, he aplicado ya el cambio y no usa el Math.pow(), ahora tiene otro metodo que cumple la misma funcion para las potencias, creo que sería mejor almacenarlo en la libreria y no en el mismo metodo, eso lo dejo a decision del grupo, lo he probado y me parece que no tiene fallos.

--- src/main/java/com/byjosep04/tema04/BinarioDecimal.java
+++ src/main/java/com/byjosep04/tema04/BinarioDecimal.java
@@ -48,17 +48,17 @@ public class BinarioDecimal {
     public static String binarioDecimal(String numero) {
         try {
             int decimal = 0;
-            int potencia = 0;
+            int numpotencia = 0;
 
             // Convertir de binario a decimal recorriendo el número desde el final, no se si es la mejor forma de hacerlo
             for (int i = numero.length() - 1; i >= 0; i--) {
                 char bit = numero.charAt(i);
                 if (bit == '1') {
-                    decimal += Math.pow(2, potencia); //Este a mi me ralla, luego lo tengo que revisar porque no entiendo bien como funciona por mucho que lo lea
+                    decimal += potencia(2, numpotencia); //Este a mi me ralla, luego lo tengo que revisar porque no entiendo bien como funciona por mucho que lo lea
                 } else if (bit != '0') {
                     return "Error: El número ingresado no es un binario válido.";
                 }
-                potencia++;
+                numpotencia++;
             }
             return "El número decimal es: " + decimal;
         } catch (Exception e) {
@@ -82,5 +82,20 @@ public class BinarioDecimal {
         }
 
         return "El número binario es: " + (binario.length() > 0 ? binario.toString() : "0");
+    }
+
+    /**
+     * Se usa en el metodo binarioDecimal ya que no se puede usar Math.pow().
+     * @author Alvaro Campos Bolufer
+     * @param base Base sobre la que se aplica el exponente
+     * @param exponente El propio exponente, las veces que se multiplicara por si misma la base.
+     * @return
+     */
+    public static int potencia(int base, int exponente) {
+        int resultado = 1;
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+        return resultado;
     }
 }
